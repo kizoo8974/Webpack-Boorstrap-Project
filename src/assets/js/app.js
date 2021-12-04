b631c63fa6b43b7589e58ffc1b658dbf3c6e8f20
Joon Kim <kizoo8974@gmail.com>
2021-12-04 10:15:45 +0900

    project setup.

---
+++ src/assets/js/app.js
@@ -0,0 +1,13 @@
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import '../css/app.css';
+// import '../scss/style.scss';
+import '../css/fontawesome-all.css';
+import 'animate.css';
+import 'lightbox2/dist/css/lightbox.min.css';
+import 'lightbox2/dist/js/lightbox-plus-jquery.js';
+
+import { wow } from './modules/wowAnimate.js';
+
